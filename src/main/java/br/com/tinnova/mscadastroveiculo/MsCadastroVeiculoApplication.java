fb55867a071a35def951b59ac36c9e3fb04a6bc6
Jonatas Coelho <jonatas.coelho@aluno.faculdadeimpacta.com.br>
2021-12-17 19:45:02 -0300

    inicial commit

---
+++ src/main/java/br/com/tinnova/mscadastroveiculo/MsCadastroVeiculoApplication.java
@@ -0,0 +1,13 @@
+package br.com.tinnova.mscadastroveiculo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCadastroVeiculoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCadastroVeiculoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tinnova/mscadastroveiculo/MsCadastroVeiculoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tinnova.mscadastroveiculo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCadastroVeiculoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
